9b1cc677fe6326895eb5611f397205af63734c66
Igor Neri de Souza <109392457+Igor29Neri@users.noreply.github.com>
2023-07-05 01:45:53 -0300

    Uso dos métodos Get e Set.

--- AppListaCurso/app/src/main/java/devandroid/igor/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/igor/applistacurso/view/MainActivity.java
@@ -10,12 +10,49 @@ import devandroid.igor.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pessoa = new Pessoa()
+        pessoa = new Pessoa();
+        pessoa.setPrimeiroNome("Igor");
+        pessoa.setSobreNome("Neri");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("11-40028922");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Cris");
+        outraPessoa.setSobreNome("Messi");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("11-99998888");
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de Contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+        int parada = 0;
+
+
     }
 }
